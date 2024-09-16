bcb2855400a6766a90a134516025a4854749ec7e
Lucas Emanuel <emanuelucaass@gmail.com>
2024-09-16 14:38:05 -0300

    sim

--- src/Aula04/Exercicios/Exercicio10.java
+++ src/Aula04/Exercicios/Exercicio10.java
@@ -8,7 +8,27 @@ public class Exercicio10 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Digite quantos reais você tem: ");
 
-        int n = sc.nextInt();
+        int real = sc.nextInt();
 
+        System.out.println("insire a opção abaixo para qual moeda você quer converter.\n");
+        System.out.println("1- dolár");
+        System.out.println("2- euro");
+        System.out.println("3- libra");
+        Scanner sc2 = new Scanner(System.in);
+        System.out.println("digite a opção: ");
+        int op= sc2.nextInt();
+
+        switch (op){
+            case 1:
+                System.out.println("o valor convertido é de "+(real/5.50+" dolares."));
+                break;
+            case 2:
+                System.out.println("o valor convertido é de "+(real/6.10+" euros"));
+                break;
+            case 3:
+                System.out.println("o valor convertido é de "+(real/7.25+" libras"));
+        }
+        sc2.close();
+        sc.close();
     }
 }
